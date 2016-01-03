92e737f8c20b27300c6863b362a114474110e1b3
Jorge Romero Iglesias <jorromigl@gmail.com>
2016-01-03 20:40:58 +0100

    commit 1

---
+++ src/main/java/security/UserAccountRepository.java
@@ -0,0 +1,11 @@
+package security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
+
+	UserAccount findByUsername(String username);
+
+}
