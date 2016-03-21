45337aae1279dbe4d7a2691db50785b6ba88bdd3
Jorge Romero Iglesias <jorromigl@gmail.com>
2016-03-21 20:22:18 +0100

    Primera version de enviar Mail
    falta añadirlo al boton recruit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Mail.xml");
+
+		MailMail mm = (MailMail) context.getBean("mailMail");
+		mm.sendMail("teamschool8@gmail.com", "belengalvez92@gmail.com", "Testing123", "Testing only \n\n Hello Spring Email Sender");
+	}
+}
