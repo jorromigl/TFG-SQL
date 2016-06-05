1a9641ebe2ea4107e9b7ec660c63cf1efd549824
Conchi Gimeno <conchigp.dh@gmail.com>
2016-06-05 18:31:11 +0200

    vistas controlador clasification

--- src/main/java/controllers/coach/ClassificationCoachController.java
+++ src/main/java/controllers/coach/ClassificationCoachController.java
@@ -1,5 +1,10 @@
 package controllers.coach;
 
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import controllers.ErrorController;
-
+import domain.Classification;
+import domain.Match;
 import services.ClassificationService;
 
 @Controller
@@ -25,17 +31,32 @@ public class ClassificationCoachController extends ErrorController {
 	public ClassificationCoachController() {
 		super();
 	}
+	
+	// List all
+			@RequestMapping(value = "/display", method = RequestMethod.GET)
+			public ModelAndView listAll() throws IOException, ParserConfigurationException, Exception {
+				ModelAndView result;
+				Collection<Classification> classifications;
+				
+				classifications = classificationService.clasification();
+										
+				result = new ModelAndView("classification/display");
+				result.addObject("classifications", classifications);
+				result.addObject("requestURI", "classification/coach/display.do");
 
-	@RequestMapping(value = "/display", method = RequestMethod.GET)
-	public ModelAndView display() {
-		ModelAndView result;
+				return result;
+			}
 
-		result = new ModelAndView("classification/display");
-
-		result.addObject("requestURI", "/classification/coach/display.do");
-
-		return result;
-	}
+//	@RequestMapping(value = "/display", method = RequestMethod.GET)
+//	public ModelAndView display() {
+//		ModelAndView result;
+//
+//		result = new ModelAndView("classification/display");
+//
+//		result.addObject("requestURI", "/classification/coach/display.do");
+//
+//		return result;
+//	}
 
 	protected ModelAndView createModelAndView() {
 
