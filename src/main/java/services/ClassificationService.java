df2e10ea5fdcf7896d2b977253349ebb146a3f08
Jorge Romero Iglesias <jorromigl@gmail.com>
2016-06-05 18:08:16 +0200

    Classificacion webScraping

--- src/main/java/services/ClassificationService.java
+++ src/main/java/services/ClassificationService.java
@@ -1,11 +1,25 @@
 package services;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.Classification;
+import domain.Coach;
+import domain.Comment;
+import domain.Folder;
 import repositories.ClassificationRepository;
 import repositories.CommentRepository;
+import security.Authority;
+import security.UserAccount;
+import webscraping.obtainData;
 
 @Service
 @Transactional
@@ -21,6 +35,33 @@ public class ClassificationService {
 	public ClassificationService() {
 		
 		super();
+	}
+	
+	public Classification create() {
+		Classification res = new Classification();
+
+		return res;
+	}
+	
+	public List<Classification> clasification() throws IOException, ParserConfigurationException, Exception{
+		Classification result= create();
+		List<Classification> tabla= new ArrayList<>();
+		List<List<String>> rows= obtainData.rows();
+		for ( List<String> row: rows){
+				result.setInfo(row.get(0));
+				result.setPoint(Integer.parseInt(row.get(1)));
+				result.setPlayed(Integer.parseInt(row.get(2)));
+				result.setWon(Integer.parseInt(row.get(3)));
+				result.setLost(Integer.parseInt(row.get(4)));
+				result.setTied(Integer.parseInt(row.get(5)));	
+				tabla.add(result);
+			}
+		
+		return tabla;
+		
+		
+		
+		
 	}
 
 }
--- src/main/java/webscraping/obtainData.java
+++ src/main/java/webscraping/obtainData.java
@@ -21,146 +21,112 @@ import org.jsoup.select.Elements;
 import org.xml.sax.InputSource;
 
 public class obtainData {
-	
-	public static final String RUTA = "./src/main/java/webscraping";
-	
-	/*Read data from a text file and store in a String variable*/
-	static String readFile(String fileName) throws IOException {
-	    BufferedReader br = new BufferedReader(new FileReader(fileName));
-	    try {
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
 
-	        while (line != null) {
-	            sb.append(line);
-	            sb.append("\n");
-	            line = br.readLine();
-	        }
-	        return sb.toString();
-	    } finally {
-	        br.close();
-	    }
+	public static final String RUTA = "./src/main/java/webscraping";
+
+	/* Read data from a text file and store in a String variable */
+	static String readFile(String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append("\n");
+				line = br.readLine();
+			}
+			return sb.toString();
+		} finally {
+			br.close();
+		}
 	}
-	
-	
-	 private static Document convertStringToDocument(String xmlStr) {
-	        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
-	        DocumentBuilder builder;  
-	        try 
-	        {  
-	            builder = factory.newDocumentBuilder();  
-	            Document doc = (Document) builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
-	            return doc;
-	        } catch (Exception e) {  
-	            e.printStackTrace();  
-	        } 
-	        return null;
-	    }
-	 
-	
-	
+
+	private static Document convertStringToDocument(String xmlStr) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			Document doc = (Document) builder.parse(new InputSource(new StringReader(xmlStr)));
+			return doc;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	public static int getStatusConnectionCode(String url) {
-		
-	    Response response = null;
-		
-	    try {
-		response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
-	    } catch (IOException ex) {
-		System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
-	    }
-	    return response.statusCode();
+
+		Response response = null;
+
+		try {
+			response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
+		} catch (IOException ex) {
+			// System.out.println("Excepción al obtener el Status Code: " +
+			// ex.getMessage());
+		}
+		return response.statusCode();
 	}
-	
+
 	/**
-	 * Con este método devuelvo un objeto de la clase Document con el contenido del
-	 * HTML de la web que me permitirá parsearlo con los métodos de la librelia JSoup
+	 * Con este método devuelvo un objeto de la clase Document con el contenido
+	 * del HTML de la web que me permitirá parsearlo con los métodos de la
+	 * librelia JSoup
+	 * 
 	 * @param url
 	 * @return Documento con el HTML
 	 */
 	public static Document getHtmlDocument(String url) {
 
-	    Document doc = null;
+		Document doc = null;
 		try {
-		    doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
-		    } catch (IOException ex) {
-			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
-		    }
-	    return doc;
+			doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+		} catch (IOException ex) {
+			// System.out.println("Excepción al obtener el HTML de la página" +
+			// ex.getMessage());
+		}
+		return doc;
 	}
-	
 
+	public static final String url = "http://www.faf.es/pnfg/NPcd/NFG_VisClasificacion?cod_primaria=1000120&codjornada=26&codcompeticion=2646493&codgrupo=3764773&codjornada=26";
+
+	public static List<List<String>> rows() throws IOException, ParserConfigurationException, Exception {
 		
-	    public static final String url = "http://www.faf.es/pnfg/NPcd/NFG_VisClasificacion?cod_primaria=1000120&codjornada=26&codcompeticion=2646493&codgrupo=3764773&codjornada=26";
-	   
-	    public static void main (String args[]) throws IOException, ParserConfigurationException, Exception {
-			
-	        // Compruebo si me da un 200 al hacer la petición
-	        if (getStatusConnectionCode(url) == 200) {
-				
-	            // Obtengo el HTML de la web en un objeto Document (solo una vez por semana)
-	            Document document = getHtmlDocument(url);
-//	            System.out.println(document);
-	            
-	            String documentHTML = readFile(RUTA + "/clasificacion05062016.txt");
-	            
-	            // Convierto el String en Document
-	    		Document doc = Jsoup.parse(documentHTML);
-	           
-	            System.out.println(doc);
-				
-	            // Busco todas las entradas que estan dentro de: 
-	         
-	            Elements entradas = doc.select("td");
-	            System.out.println("Número de entradas en la página inicial de Jarroba: "+entradas.size()+"\n");
-				List<String> equipos = new ArrayList<String>();
-				List<String> resultados = new ArrayList<String>();
-	            // Paseo cada una de las entradas
-	            for (Element elem : entradas) {
-	                String equipo = elem.getElementsByClass("fondotitulolistadosizq").text();
-	                equipos.add(equipo);
-	                String Listados = elem.getElementsByClass("fondolistados").text();
-					resultados.add(Listados);
-					
-			
-					
-					// Busco todas las entradas que estan dentro de: 
-			         
-//		            Elements filas = doc.select("tr");
-//					List<String> filaList = new ArrayList<String>();
-//					Element firstRow = filas.first();
-//					
-//					List<String> headers = new ArrayList<String>();
-//				
-//					
-//					
-//					System.out.println("FILAS" + filas);
-//					
-//		            for (Element elem2 : filas) {
-////		                String orden = 
-//		                filaList.add(elem2.getElementsByClass("FILA_OFF").text());
-//		               
-//	            }
-//	            System.out.println("FILAS_OFF" + filaList);
-	            
-	            }
-	            
-	    		System.out.println("QEUIPO" + equipos);
-				System.out.println("Result" + resultados);
-				
-	        }else{
-	            System.out.println("El Status Code no es OK es: "+getStatusConnectionCode(url));
-	        }
-	        
-	      
-	    	
-	    }
+		List<List<String>> rows = new ArrayList<List<String>>();
+		
+		// Compruebo si me da un 200 al hacer la petición
+		if (getStatusConnectionCode(url) == 200) {
+
+			// Obtengo el HTML de la web en un objeto Document (solo una vez por
+			// semana)
+			Document document = getHtmlDocument(url);
+			// System.out.println(document);
+
+			String documentHTML = readFile(RUTA + "/clasificacion05062016.txt");
+
+			// Convierto el String en Document
+			Document doc = Jsoup.parse(documentHTML);
+			Elements tables = doc.getElementsByTag("table");
+			Element firstTable = tables.get(4);
+			Elements tr2 = firstTable.getElementsByTag("tr");
+			List<String> lista = new ArrayList<String>();
+			int numeroRows = 0;
+			for (int i = 2; i < 15; i++) {
+				Element td = tr2.get(i);
+				Elements tds = td.getElementsByTag("td");
+				numeroRows = tds.size();
+				for (int e = 2; e < numeroRows; e++) {
+					lista.add(tds.get(e).text());
+				}
+			}
+			int a = 0;
+			int b = 10;
+			for (int c = 0; c < numeroRows; c++) {
+				rows.add(lista.subList(a, b));
+				a = b;
+				b += 10;
+			}
+		}
+		return rows;
+	}
 }
-	    	
-	    	
-	
-
-
-
-
-
-
